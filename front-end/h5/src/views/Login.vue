7f9ea1f61e904b28679b861a28712d6ff51efb28
lengshengren <379306634@qq.com>
2020-03-27 14:46:45 +0800

    fix bug
    fix bug

--- front-end/h5/src/views/Login.vue
+++ front-end/h5/src/views/Login.vue
@@ -74,7 +74,7 @@ export default {
       const that = this;
       axios({
         method: "post",
-        url: "https://api.gaojihealth.cn/api/loginbyPhonepass",
+        url: "",
         data: {
           phone: phone,
           password: password,
